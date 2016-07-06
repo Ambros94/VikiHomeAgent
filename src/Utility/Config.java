b2ed3fc21789fa66d25ed8fa06350dc5a32a8ee8
Ambros94 <luca_ambro94@hotmail.it>
2016-07-06 14:30:57 +0200

    Added default config values

--- src/Utility/Config.java
+++ src/Utility/Config.java
@@ -1,5 +1,8 @@
 package Utility;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -20,6 +23,10 @@ public class Config {
     private String rightFilePath;
     private String vikiAddress;
     private String dictionaryPath;
+    /**
+     * Logger
+     */
+    private static Logger logger = LoggerFactory.getLogger(Config.class);
 
     private Config() {
         try {
@@ -27,9 +34,25 @@ public class Config {
             Properties props = new Properties();
             props.load(file);
             wrongFilePath = props.getProperty("wrongFilePath");
+            if (wrongFilePath == null) {
+                logger.info("No wrongFile path in the config file, using default [resources/commandLog/wrong.txt]");
+                wrongFilePath = "resources/commandLog/wrong.txt";
+            }
             rightFilePath = props.getProperty("rightFilePath");
+            if (rightFilePath == null) {
+                logger.info("No rightFilePath path in the config file, using default [resources/commandLog/right.txt]");
+                rightFilePath = "resources/commandLog/right.txt";
+            }
             vikiAddress = props.getProperty("vikiAddress");
+            if (vikiAddress == null) {
+                logger.info("No vikiAddress path in the config file, using default [localhost:1234]");
+                vikiAddress = "localhost:1234";
+            }
             dictionaryPath = props.getProperty("dictionaryPath");
+            if (dictionaryPath == null) {
+                logger.info("No dictionaryPath path in the config file, using default [resources/dict]");
+                dictionaryPath = "resources/dict";
+            }
         } catch (Exception e) {
             System.out.println("error" + e);
         }
